e04c920c99aa8d6f7653f3cf87891cadee8ae726
Bartek <bartlomiej.czapla@globallogic.com>
2020-10-27 17:35:52 +0100

    Init Unity IoC Container

--- BasicXamarin/BasicXamarin.Android/MainActivity.cs
+++ BasicXamarin/BasicXamarin.Android/MainActivity.cs
@@ -14,12 +14,15 @@ using System.IO;
 using Xamarin.Forms;
 using XamarinBasic.Core;
 using Android.Support.V7.App;
+using Unity;
+using CommonServiceLocator;
+using Unity.ServiceLocation;
 
 namespace BasicXamarin.Droid
 {
     [Activity(Theme = "@style/MainTheme",
               ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
-    public class MainActivity : AppCompatActivity//global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -30,23 +33,8 @@ namespace BasicXamarin.Droid
             var webView = FindViewById<Android.Webkit.WebView>(Resource.Id.customWebView);
             var customWebView = new CustomWebView(webView);
             customWebView.LoadUrl("http://any/index.html");
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-    public class AndroidInitializer : IPlatformInitializer
-    {
-        public void RegisterTypes(IContainerRegistry containerRegistry)
-        {
-            // Register any platform specific implementations
-            containerRegistry.RegisterSingleton<IPlatformSettingsProvider, AndroidSettingsProvider>();
-            containerRegistry.Register(typeof(IRepository), typeof(Repository));
+            var container = ServiceRegistry.BuildContainer();
+            ServiceRegistry.SetLocatorProvider(container);
         }
     }
 }
---
+++ BasicXamarin/BasicXamarin.Android/ServiceRegistry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CommonServiceLocator;
+using Unity;
+using Unity.Lifetime;
+using Unity.ServiceLocation;
+using XamarinBasic.Core;
+
+namespace BasicXamarin.Droid
+{
+    public static class ServiceRegistry
+    {
+        public static UnityContainer BuildContainer()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IPlatformSettingsProvider, AndroidSettingsProvider>(Singleton);
+            //containerRegistry.Register(typeof(IRepository), typeof(Repository));
+
+            return container;
+        }
+
+        private static ContainerControlledLifetimeManager Singleton =>
+            new ContainerControlledLifetimeManager();
+
+        public static void SetLocatorProvider(IUnityContainer container)
+        {
+            ServiceLocator.SetLocatorProvider(() =>
+                 new UnityServiceLocator(container));
+        }
+    }
+}
